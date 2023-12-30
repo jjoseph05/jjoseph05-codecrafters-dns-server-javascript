74ffc5863b0108fc7e52b334a3f94145d7e70c88
Justin <j.joseph@ant2.jp>
2023-12-30 11:24:02 +0900

    adds question and updates byffer, stage 3

--- app/main.js
+++ app/main.js
@@ -8,25 +8,41 @@ console.log("Logs from your program will appear here!");
  const udpSocket = dgram.createSocket("udp4");
  udpSocket.bind(2053, "127.0.0.1");
 
- udpSocket.on("message", (buf, rinfo) => {
+function encodeDomainName(domain) {
+  const labels = domain.split('.');
+  const buffer = Buffer.alloc(domain.length + 2);
+  let offset = 0;
+
+  labels.forEach(label => {
+    buffer.writeUInt8(label.length, offset++);
+    buffer.write(label, offset);
+    offset += label.length;
+  });
+
+  buffer.writeUInt8(0, offset);
+  return buffer;
+}
+
+udpSocket.on("message", (buf, rinfo) => {
    try {
-     const response = Buffer.alloc(12);
+     const header = Buffer.alloc(12);
 
-     response.writeUInt16BE(0x04D2, 0);
+     header.writeUInt16BE(0x04D2, 0);
 
-     response[2] |= 0b10000000;
+     header[2] |= 0b10000000;
+     const questionBuffer = encodeDomainName('codecrafters.io');
 
-     response.writeUInt16BE(0x0000, 4);
-
-
-     response.writeUInt16BE(0x0000, 6);
+     header.writeUInt16BE(0x0001, 4);
 
 
-     response.writeUInt16BE(0x0000, 8);
+     header.writeUInt16BE(0x0000, 6);
 
 
-     response.writeUInt16BE(0x0000, 10);
+     header.writeUInt16BE(0x0000, 8);
 
+
+     header.writeUInt16BE(0x0000, 10);
+     const response = Buffer.concat([header, questionBuffer]);
      udpSocket.send(response, rinfo.port, rinfo.address);
    } catch (e) {
      console.log(`Error receiving data: ${e}`);
